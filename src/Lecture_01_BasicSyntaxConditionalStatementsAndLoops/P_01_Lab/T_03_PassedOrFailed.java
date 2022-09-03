336f22961b4d041b866b4c5c087d0422a7241b8e
Kremena <kremidimitrova0@gmail.com>
2022-09-03 15:24:21 +0300

    commit: basic syntax lab

---
+++ src/Lecture_01_BasicSyntaxConditionalStatementsAndLoops/P_01_Lab/T_03_PassedOrFailed.java
@@ -0,0 +1,17 @@
+package Lecture_01_BasicSyntaxConditionalStatementsAndLoops.P_01_Lab;
+
+import java.util.Scanner;
+
+public class T_03_PassedOrFailed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        if (grade >= 3.00) {
+            System.out.println("Passed!");
+        } else {
+            System.out.println("Failed!");
+        }
+    }
+}
