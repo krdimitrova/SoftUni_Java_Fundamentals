c5ad61bd5c8565d0f80a781323e4667379414f75
Kremena <kremidimitrova0@gmail.com>
2022-09-06 14:12:57 +0300

    commit: basic syntax lab

---
+++ src/Lecture_01_BasicSyntaxConditionalStatementsAndLoops/P_01_Lab/T_06_ForeignLanguages.java
@@ -0,0 +1,25 @@
+package Lecture_01_BasicSyntaxConditionalStatementsAndLoops.P_01_Lab;
+
+import java.util.Scanner;
+
+public class T_06_ForeignLanguages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String country = scanner.nextLine();
+
+        switch (country) {
+            case "England":
+            case "USA":
+                System.out.println("English");
+                break;
+            case "Spain":
+            case "Argentina":
+            case "Mexico":
+                System.out.println("Spanish");
+                break;
+            default:
+                System.out.println("unknown");
+        }
+    }
+}
